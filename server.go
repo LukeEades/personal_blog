9b1ca691557854fa45d04e0535430d4144d190cf
Luke Eades <lukeeadeswork@gmail.com>
2024-11-06 16:40:23 -0800

    added fileserving capabilities

--- server.go
+++ server.go
@@ -11,11 +11,12 @@ import (
 )
 
 type Article struct {
-	Title   string
-	Author  string
-	Text    string
-	Created time.Time
-	Updated time.Time
+	Title       string
+	Author      string
+	Description string
+	Text        string
+	Created     time.Time
+	Updated     time.Time
 }
 
 const url string = ":8080"
@@ -26,28 +27,62 @@ var articleTemplate *template.Template
 var homeTemplate *template.Template
 var dashTemplate *template.Template
 
-func main() {
-	errorTemplate, templateError = template.ParseFiles("./error.template.html")
-	if templateError != nil {
-		log.Fatal(templateError)
-	}
-	articleTemplate, templateError = template.ParseFiles("./article.template.html")
-	if templateError != nil {
-		log.Fatal(templateError)
-	}
-	homeTemplate, templateError = template.ParseFiles("./home.template.html")
-	if templateError != nil {
-		log.Fatal(templateError)
-	}
-	dashTemplate, templateError = template.ParseFiles("./dash.template.html")
-	if templateError != nil {
-		log.Fatal(templateError)
-	}
+// note: has empty text field
+var fileInfo []Article
 
+var templates string = "./files/templates/"
+var filePath string = "./files/static/"
+
+func main() {
+	errorTemplate, templateError = template.ParseFiles(templates + "error.template.html")
+	if templateError != nil {
+		log.Fatal(templateError)
+	}
+	articleTemplate, templateError = template.ParseFiles(templates + "article.template.html")
+	if templateError != nil {
+		log.Fatal(templateError)
+	}
+	homeTemplate, templateError = template.ParseFiles(templates + "home.template.html")
+	if templateError != nil {
+		log.Fatal(templateError)
+	}
+	dashTemplate, templateError = template.ParseFiles(templates + "dash.template.html")
+	if templateError != nil {
+		log.Fatal(templateError)
+	}
+	fileInfo = loadFileData()
+	// TODO: need to configure a file server/handler to serve static files when needed from the server
+
+	http.HandleFunc("/files/static/{file}", handleFile)
 	http.HandleFunc("/{path}", handleHome)
 	http.HandleFunc("/", handleHome)
 	http.HandleFunc("/article/{name}", handleRegArt)
 	log.Fatal(http.ListenAndServe(url, nil))
+}
+
+func loadFileData() []Article {
+	files, err := os.ReadDir("./articles")
+	if err != nil {
+		log.Fatal(err)
+	}
+	articles := make([]Article, 0, 5)
+	for _, file := range files {
+		bytes, err := os.ReadFile("./articles/" + file.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		// unmarshall all json
+		var art Article
+		json.Unmarshal(bytes, &art)
+		art.Text = ""
+		articles = append(articles, art)
+	}
+	return articles
+}
+
+func handleFile(w http.ResponseWriter, r *http.Request) {
+	fileName := r.PathValue("file")
+	http.ServeFile(w, r, filePath+fileName)
 }
 
 // handles request for homepage
@@ -81,17 +116,13 @@ func saveJson(fileName string, art Article) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	os.WriteFile("./files/"+fileName+".json", data, 0777)
+	os.WriteFile("./articles/"+fileName+".json", data, 0777)
 }
 
 // sends home template to client
 // should rework this to use stored map values
 func getRegHome(w http.ResponseWriter) {
-	files, err := os.ReadDir("./files")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = homeTemplate.Execute(w, files)
+	err := homeTemplate.Execute(w, fileInfo)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -99,7 +130,7 @@ func getRegHome(w http.ResponseWriter) {
 
 // returns parsed json in article format from file
 func getArticle(fileName string) (Article, error) {
-	file, err := os.ReadFile("./files/" + fileName + ".json")
+	file, err := os.ReadFile("./articles/" + fileName + ".json")
 	// need to handle file not existing here
 	var art Article
 	if err != nil {
